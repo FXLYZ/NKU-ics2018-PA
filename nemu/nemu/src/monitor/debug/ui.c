e719fb09dad617d4e0cbf48a5124a276d260d1df
tracer-ics2017 <tracer@njuics.org>
2020-06-04 21:17:10 -0700

    >  run 1410576 cengzhitao Linux ubuntu 4.15.0-76-generic #86~16.04.1-Ubuntu SMP Mon Jan 20 10:58:26 UTC 2020 i686 i686 i686 GNU/Linux  21:17:10 up 1 day,  6:39,  1 user,  load average: 0.18, 0.25, 0.25 211417b9e3fe4be5fbea18e862bc9c62e7d32314

---
+++ nemu/nemu/src/monitor/debug/ui.c
@@ -0,0 +1,236 @@
+#include "monitor/monitor.h"
+#include "monitor/expr.h"
+#include "monitor/watchpoint.h"
+#include "nemu.h"
+
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void cpu_exec(uint64_t);
+
+/* We use the `readline' library to provide more flexibility to read from stdin. */
+char* rl_gets() {
+  static char *line_read = NULL;
+
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
+
+  line_read = readline("(nemu) ");
+
+  if (line_read && *line_read) {
+    add_history(line_read);
+  }
+
+  return line_read;
+}
+
+static int cmd_c(char *args) {
+  cpu_exec(-1);
+  return 0;
+}
+
+static int cmd_q(char *args) {
+  return -1;
+}
+
+static int cmd_help(char *args);
+
+static int cmd_si(char *args);
+static int cmd_info(char *args);
+static int cmd_x(char *args);
+static int cmd_p(char *args);
+static int cmd_w(char *args){
+   new_wp(args);
+   return 0;
+}
+
+static int cmd_d(char* args){
+   int num=0;
+   int nret=sscanf(args,"%d",&num);
+   if(nret<=0){
+       printf("args error in cmd_si\n");
+       return 0;
+   }
+   int r=free_wp(num);
+   if(r==false)
+       printf("error: no watchpoint %d\n",num);
+   else
+       printf("success delete watchpoint %d\n",num);
+   return 0;
+}
+
+
+static struct {
+  char *name;
+  char *description;
+  int (*handler) (char *);
+} cmd_table [] = {
+  { "help", "Display informations about all supported commands", cmd_help },
+  { "c", "Continue the execution of the program", cmd_c },
+  { "q", "Exit NEMU", cmd_q },
+  {"si","args:[N];execute[N] instructions step by step",cmd_si},
+  {"info","args:r/w;print information about registers or watchpoint",cmd_info},
+  {"x","x [N] [EXPR]:scan the memory",cmd_x},
+  {"p","expr",cmd_p},
+  {"d","delete the watchpoint",cmd_d},
+  {"w","set watchpoint",cmd_w},
+
+  /* TODO: Add more commands */
+};
+
+#define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+
+static int cmd_si(char *args){
+	uint64_t N=0;
+	if(args==NULL)
+		N=1;
+	else{
+		int nRset=sscanf(args,"%llu",&N);
+		if(nRset<=0){
+			printf("args error in cmd_si\n");
+			return 0;
+		}
+	}
+	cpu_exec(N);
+	return 0;
+}
+
+static int cmd_info(char *args){
+	char s;
+	if(args==NULL){
+		printf("args error in cmd_info\n");
+		return 0;
+	}
+        int nRet=sscanf(args,"%c",&s);
+	if(nRet<=0){
+		printf("args error in cmd_info\n");
+		return 0;
+	}
+	if(s=='r'){
+		int i;
+		//32 reg
+		for(i=0;i<8;i++)
+			printf("%s   0x%x\n",regsl[i],reg_l(i));
+		printf("eip  0x%x\n",cpu.eip);
+		//16 reg
+		for(i=0;i<8;i++)
+			printf("%s   0x%x\n",regsl[i],reg_w(i));
+		//8 reg
+		for(i=0;i<8;i++)
+			printf("%s   0x%x\n",regsl[i],reg_b(i));
+		return 0;
+      }
+      if(s=='w'){
+           print_wp();
+           return 0;
+      } 
+      printf("args error in cmd_info\n");
+      return 0;
+}
+
+static int cmd_x(char *args){
+    int nLen = 0;
+    bool success;
+    int j; for (j = 0; args[j] != ' '; j++);
+    char* e = (char*)malloc(strlen(args + j + 1));  // expression
+
+    int nRet = sscanf(args, "%d %s", &nLen, e);
+    if (nRet <= 0) {
+        printf("args error in cmd_x\n");
+        return 0;
+    }
+
+    vaddr_t addr = expr(e, &success);
+    if (success == false) {
+        printf("error in expr()\n");
+        return 0;
+    }
+
+    printf("Memory from %d(0x%x): ", addr, addr);
+    int i;
+    for (i = 0; i < nLen; i++) {
+        if (i % 4 == 0)
+            printf("\n0x%x:  0x%02x", addr + i, vaddr_read(addr + i, 1));
+        else
+            printf("  0x%02x", vaddr_read(addr + i, 1));
+    }
+    printf("\n");
+    return 0;
+}
+
+static int cmd_p(char *args){
+	bool success;
+	int res=expr(args,&success);
+	if(success==false)
+		printf("error in expr()\n");
+	else
+		printf("the value of expr is:%d\n",res);
+	return 0;
+}
+
+
+static int cmd_help(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  int i;
+
+  if (arg == NULL) {
+    /* no argument given */
+    for (i = 0; i < NR_CMD; i ++) {
+      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    }
+  }
+  else {
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        return 0;
+      }
+    }
+    printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
+
+void ui_mainloop(int is_batch_mode) {
+  if (is_batch_mode) {
+    cmd_c(NULL);
+    return;
+  }
+
+  while (1) {
+    char *str = rl_gets();
+    char *str_end = str + strlen(str);
+
+    /* extract the first token as the command */
+    char *cmd = strtok(str, " ");
+    if (cmd == NULL) { continue; }
+
+    /* treat the remaining string as the arguments,
+     * which may need further parsing
+     */
+    char *args = cmd + strlen(cmd) + 1;
+    if (args >= str_end) {
+      args = NULL;
+    }
+
+#ifdef HAS_IOE
+    extern void sdl_clear_event_queue(void);
+    sdl_clear_event_queue();
+#endif
+
+    int i;
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(cmd, cmd_table[i].name) == 0) {
+        if (cmd_table[i].handler(args) < 0) { return; }
+        break;
+      }
+    }
+
+    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+  }
+}
+
