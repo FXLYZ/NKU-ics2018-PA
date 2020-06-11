e3d44bdc29ffccb46e231da797a15423f1d991b3
tracer-ics2017 <tracer@njuics.org>
2020-06-11 00:22:48 -0700

    >  run 1410576 cengzhitao Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  00:22:48 up  3:29,  1 user,  load average: 0.27, 0.39, 0.29 bf0100a0b8e9a5e649b1f31f73aff0d4a19afffe

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "fs.h"
+#include "memory.h"
 
 #define DEFAULT_ENTRY ((void *)0x80480000)
 
@@ -11,7 +12,25 @@ extern void ramdisk_read(void *buf, off_t* offset, size_t len);
 uintptr_t loader(_Protect *as, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   Log("filename=%s, fd=%d", filename, fd);
-  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+
+  //分页机制实现后增加的部分
+  int size=fs_filesz(fd);
+  int ppnum=size/PGSIZE;
+  if (size%PGSIZE!=0)
+  {
+    ppnum++;
+  }
+  void *pa=NULL;
+  void *va=DEFAULT_ENTRY;
+  for (int i = 0; i < ppnum; i++)
+  {
+    pa=new_page();
+    _map(as,va,pa);
+    fs_read(fd,pa,PGSIZE);
+    va+=PGSIZE;
+  }
+
+  //fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
