462e761dd97e413c8ba561f07ff4f4e8caceb172
tracer-ics2017 <tracer@njuics.org>
2020-06-10 23:18:55 -0700

    >  compile 1410576 cengzhitao Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  23:18:55 up  2:25,  1 user,  load average: 0.27, 0.13, 0.12 78aa23a1dd83d06581afb169d1f733bf3f2016c0

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,7 +1,15 @@
 #include "nemu.h"
 #include "device/mmio.h"
+#include "memory/mmu.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
+
+//获取pte
+#define PTE_ADDR(pte)  ((uint32_t)(pte)&~0xfff)
+//解析va
+#define PDX(va) (((uint32_t)(va)>>22)&0x3ff)
+#define PTX(va) (((uint32_t)(va)>>12)&0x3ff)
+#define OFF(va) ((uint32_t)(va)&0xfff)
 
 #define pmem_rw(addr, type) *(type *)({\
     Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
@@ -27,11 +35,70 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
   else
       mmio_write(addr, len, data, r);
 }
-
+/* 
 uint32_t vaddr_read(vaddr_t addr, int len) {
   return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
   paddr_write(addr, len, data);
+}*/
+
+//查表转换地址
+paddr_t page_translate(vaddr_t addr,bool iswrite){
+  CR0 cr0=(CR0)cpu.CR0;
+  if (cr0.paging&&cr0.protect_enable)
+  {
+    /* code */
+    CR3 cr3=(CR3)cpu.CR3;
+
+    //设置PDE
+    PDE* pgdirs=(PDE*)PTE_ADDR(cr3.val);
+    PDE pde=(PDE)paddr_read((uint32_t)(pgdirs+PDX(addr)),4);
+    Assert(pde.present,"addr=0x%x",addr);
+
+    //PTE
+    PTE* ptab=(PTE*)PTE_ADDR(pde.val);
+    PTE pte=(PTE)paddr_read((uint32_t)(ptab+PTX(addr)),4);
+    Assert(pte.present,"addr=0x%x",addr);
+
+    //accessed,dirty
+    pde.accessed=1;
+    pte.accessed=1;
+    if (iswrite)
+    {
+      pte.dirty=1;
+    }
+
+    //根据偏移量转化为物理地址
+    paddr_t paddr=PTE_ADDR(pte.val)| OFF(addr);
+    return paddr;
+  }
+  return addr;
+}
+
+uint32_t vaddr_read(vaddr_t addr,int len){
+  if (PTE_ADDR(addr)!=PTE_ADDR(addr+len-1))
+  {
+    printf("error:跨页，数据在两页page中addr=0x%x,len=%d",addr,len);
+    assert(0);
+  }
+  else
+  {
+    paddr_t paddr=page_translate(addr,false);
+    return paddr_read(paddr,len);
+  }
+}
+
+void vaddr_write(vaddr_t addr,int len,uint32_t data){
+  if (PTE_ADDR(addr)!=PTE_ADDR(addr+len-1))
+  {
+    printf("error:跨页，数据在两页page中addr=0x%x,len=%d",addr,len);
+    assert(0);
+  }
+  else
+  {
+    paddr_t paddr=page_translate(addr,true);
+    return paddr_write(paddr,len,data);
+  }
 }
